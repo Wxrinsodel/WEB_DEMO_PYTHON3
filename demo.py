e24672c4158dd9141fa5aaf12505d979f0747606
Supawadee <150314053+Wxrinsodel@users.noreply.github.com>
2025-01-28 13:28:11 +0700

    DEMO

--- demo.py
+++ demo.py
@@ -1,9 +1,35 @@
 from flask import Flask, request
+from flask import render_template
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route("/test/<value1>/<value2>")
 def test_square(value1, value2):
     result = int(value1)**2 + int(value2)**2
-    
+
     return f"<p> Answer is: {result}</p>"
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    value = int(request.form['number'])
+    return render_template("calculator.html", result=value**2)
+
+@app.route("/plot", methods=["GET", "POST"])
+def plot():
+    if request.method == "POST":
+        xleft = int(request.form["xleft"])
+        xright = int(request.form["xright"])
+
+        x = np.linspace(xleft, xright, 100)
+        y = np.sin(x)
+
+        plt.plot(x, y)
+
+        image_name = f"static/images/plot.png"
+        plt.savefig(image_name)
+        plt.close()
+        return render_template("plotter.html", image_path=image_name)
+    else:
+        return render_template("plotter.html")
